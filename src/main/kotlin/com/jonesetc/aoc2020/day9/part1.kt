2da15791b01bc474a55df25f489a63e10cd1e5ed
Kyle Jones <kyle@jonesetc.com>
2020-12-09 20:46:27 -0700

    day 9

--- src/main/kotlin/com/jonesetc/aoc2020/day9/part1.kt
+++ src/main/kotlin/com/jonesetc/aoc2020/day9/part1.kt
@@ -1,6 +1,22 @@
 @file:JvmName("Part1")
 package com.jonesetc.aoc2020.day9
 
+import com.jonesetc.aoc2020.inputFile
+
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    println("prep for first part done")
+    inputFile(9)
+        .readLines()
+        .map(String::toLong)
+        .windowed(26)
+        .map { Pair(it.dropLast(1).toSet(), it.last()) }
+        .dropWhile { (available, current) ->
+            available
+                .any { old ->
+                    val compliment = current - old
+                    compliment != old && compliment in available
+                }
+        }
+        .take(1)
+        .map { it.second }
+        .forEach(::println)
 }
--- src/main/kotlin/com/jonesetc/aoc2020/day9/part2.kt
+++ src/main/kotlin/com/jonesetc/aoc2020/day9/part2.kt
@@ -1,6 +1,40 @@
 @file:JvmName("Part2")
 package com.jonesetc.aoc2020.day9
 
+import com.jonesetc.aoc2020.inputFile
+
 fun main(@Suppress("UNUSED_PARAMETER") args: Array<String>) {
-    println("prep for second part done")
+    val numbers = inputFile(9)
+        .readLines()
+        .map(String::toLong)
+
+    val target = numbers
+        .windowed(26)
+        .map { Pair(it.dropLast(1).toSet(), it.last()) }
+        .dropWhile { (available, current) ->
+            available
+                .any { old ->
+                    val compliment = current - old
+                    compliment != old && compliment in available
+                }
+        }
+        .take(1)
+        .map { it.second }
+        .first()
+
+    numbers.indices
+        .flatMap { start ->
+            numbers
+                .drop(start)
+                .scan(listOf<Long>(), { acc, curr ->
+                    acc.plus(curr)
+                })
+                .dropWhile { subList -> subList.sum() < target }
+                .take(1)
+                .filter { subList -> subList.sum() == target }
+                .map { sublist -> sublist.sorted() }
+                .map { sortedSubList -> sortedSubList.first() + sortedSubList.last() }
+        }
+        .take(1)
+        .forEach(::println)
 }
